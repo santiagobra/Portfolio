3b4577d9e8d9f1f50f3db3bc04e2bc7c450daf8a
katumba12 <81330522+katumba12@users.noreply.github.com>
2022-09-30 17:57:55 -0300

    Publicacion

---
+++ index.php
@@ -0,0 +1,342 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+    <script src="assets/js/main.js"></script>
+<!-- pre loader  -->
+ 
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Santiago<span>Bra.</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">Sobre Mi</a></li>
+                <li><a href="#services" class="menu-btn">Servicios</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#teams" class="menu-btn">Portfolio</a></li>
+                <li><a href="#contact" class="menu-btn">Contacto</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hola, mi nombre es</div>
+                <div class="text-2">Santiago Brassesco </div>
+                <div class="text-3">y soy <span class="typing"></span></div>
+                <div class="classbox">
+                <a href="#about">Sobre Mi</a>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">Sobre mi</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="assets/img/yo.jpg" alt="img">
+                </div>
+                <div class="column right">
+                    <div class="text">Soy Santi, y soy un <span class="typing-2"></span></div>
+                    <p> Un joven entusiasta en busca de nuevos desafíos y retos profesionales, en el área de desarrollo Web,
+                         tanto Front-end  como Back-end. Considero fundamental el trabajo en equipo, para alcanzar un buen resultado 
+                         y brindar una buena experiencia a los clientes y usuarios. En mis 4 años de desarrollo, 
+                         el gran pilar de mis proyectos es el lenguaje PHP, y en mi desarrollo Front-end poseo una sólida experiencia en HTML5, CSS y BOOSTRAP 
+                         en sus versiones más actuales.  Me mantengo en formación constante de manera formal y autodidacta, 
+                         y es por ello que también estoy incluyendo en mis proyectos JavaScript, y me encuentro capacitandome tambien en los lenguajes Python y Ruby.
+                          También cuento con un gran conocimiento  y aplicación  de base de datos en SQL Server y MySQL. Por último,
+                           y no menos importante, este año comencé formaciones formales en Data Analytics, 
+                        ya que ha despertado mi curiosidad ampliamente, y considero un gran complemento para mi trabajo diario como programador.
+                         </p>
+                    <a href="https://drive.google.com/file/d/1iXEoG9RG6KFEMSZ7Xnl36aCS-E7A4DNw/view?usp=sharing" target="_blank" download="cv/sample.pdf">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+     <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Mis Servicios</h2>
+            <div class="serv-content">
+            </div>
+        </div>
+    <div class="container">
+        <div class="box">
+            <span id="first"> </span>
+            <div class="content">
+                <h2>Desarrollador Web</h2>
+                <p>Realizo diversos proyectos, adaptados a los requisitos de los solicitantes tanto lo visual como en la programación.</p>
+            </div>
+        </div>
+        <div class="box">
+            <span id="second"></span>
+            <div class="content">
+                <h2>Programador PHP</h2>
+                <p>Las web y sistemas están hechas en este lenguaje, y cuentan, en su mayoría, con un “Admin” o “Sistema de Gestión” a través del cual pueden administrar su sitio.</p>
+            </div>
+        </div>
+        <div class="box">
+            <span id="third"></span>
+            <div class="content">
+                <h2>Data Analytics</h2>
+                <p>Actualmente, me encuentro formándome en Data Analystics, a través de CODERHOUSE y de COURSERA una certificación de GOOGLE, con el propósito de complementar mis conocimiento como informático. </p>
+            </div>
+        </div>
+        <div class="box">
+            <span id="fourth"></span>
+            <div class="content">
+                <h2>Freelancer</h2>
+                <p>Disfruto de todo el proceso end to end que mi trabajo requiere, desde del primer contacto con el cliente hasta la entrega del proyecto final. Cada sistema o web enriquece mis capacidades.</p>
+            </div>
+        </div>
+        <!--<div class="box">
+            <span id="fifth"></span>
+            <div class="content">
+                <h2>Digital Marketing</h2>
+                <p>Digital Marketing is the only tools which helps people to build advertised there business digitally which make people more attracted to there products and there business.</p>
+            </div>
+        </div>
+        <div class="box">
+            <span id="sixth"></span>
+            <div class="content">
+                <h2>App Development</h2>
+                <p>Apps helps to those people who are not mostly friendly towards the website.Apps helps people in user interface which make them easier.</p>
+            </div>
+        </div> -->
+    </div>
+</section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">Mis habilidades y Experiencia.</div>
+                    <p>Desde que comence mi viaje como desarrollador independiente hace casi 4 años, he realizado trabajos remotos para Pymes, Asociaciones Civiles, Instituciones Educativas, entre otras.
+                        Creo sitios Web responsivos que se adaptan a todo tipo de dispositivos, funcionales y cumpliendo todas las expectativas de los solicitantes. El área principal de mi desarrollo esta en la 
+                        logica que se implementa para cada Web, teniendo un contacto cercano con los solicitantes para llegar a sy fin deseado. Usando en el Front-end: <b>HTML</b>, <b>CSS</b>, <b>BOOSTRAP</b> y <b>JS</b>. Y en el 
+                        Back-end: <b>PHP</b> puro, con una base solida SQL generalmente utilizando el gestor <b>MySql</b> para utilizar los datos. Y en algunos casos usando complementos PHP como FPDF(generar PDF personalizados)
+                        entre otros. 
+    
+                        <br><br>
+                        Tambien tengo amplios conocimientos en <b>TECNOLOGIA</b> en general y actualmente me ecuentro realizando cursos certificados de <b>DATA ANALYTICS</b>.
+                        <br> 
+                        Visita mi<a href="https://www.linkedin.com/in/santiago-brassesco12/" target="-blank" id="link">LinkedIn</a>para mas detalles.
+
+                    </p>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>90%</span>
+                        </div>
+                         <div class="line php"></div>
+                    </div>
+                <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Boostrap</span>
+                            <span>85%</span>
+                        </div>
+                        <div class="line wordpress"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySql</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- project section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">Mis Proyectos</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="https://images.pexels.com/photos/1109543/pexels-photo-1109543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img">
+                        <div class="text">Sistema Gestion Administrativa</div>
+                        <p>Sistema a medida desarrollado en PHP. </p>
+                        <a href="" class="project" target="_blank">Ver Proyecto</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="https://images.pexels.com/photos/102127/pexels-photo-102127.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img">
+                        <div class="text">Portfolio de <br>Artista</div>
+                        <p>Desarrollado y diseñado para Muralista.</p>
+                        <a href="https://eriv.com.ar" class="project" target="_blank">Ver Proyecto</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="https://images.pexels.com/photos/47730/the-ball-stadion-football-the-pitch-47730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img">
+                        <div class="text">Web & Sistema de Torneo</div>
+                        <p>Sistema y web a medida para Torneos Deportivos.</p>
+                        <a href="http://onegroupeventos.com.ar" class="project" target="_blank">Ver Proyecto</a>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="https://images.pexels.com/photos/256395/pexels-photo-256395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="img">
+                        <div class="text">Sistema Gestion Academica</div>
+                        <p>Sistema a medida desarrollado en PHP.</p>
+                        <a href="" class="project" target="_blank">Ver Proyecto</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contactarme</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Ponerse en contacto</div>
+                    <p>
+                    Si estás interesado en que trabajemos juntos? Completa el formulario a un lado con información sobre su proyecto y me pondré en contacto con ustedes lo antes posible.
+                    </p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Nombre</div>
+                                <div class="sub-title">Santiago Brassesco</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Dirección</div>
+                                <div class="sub-title">Paraná, Entre Ríos, Argentina</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">santiagobra@outlook.es</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Dejame un Mensaje</div>
+                    <form action="#">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Nombre" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Tema" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Mensaje..." required></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button type="submit">Enviar Mensaje</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+  
+    <footer>
+        <div class="waves">
+            <div class="wave" id="wave1"></div>
+            <div class="wave" id="wave2"></div>
+            <div class="wave" id="wave3"></div>
+            <div class="wave" id="wave4"></div>
+        </div>
+        <h2><span class="typing-3"></span></h2>
+        <ul id="media">
+            <!-- Linkedin  -->
+            <li style="--clr:#0011ff;">
+                <a href="https://www.linkedin.com/in/santiago-brassesco12/" target="-blank"><i class="fa-brands fa-linkedin"></i>
+                </a>
+            </li>
+           
+            <!-- Instagram  -->
+            <li style="--clr:#e73f0c;">
+                <a href="https://www.instagram.com/santiagobrassesco/" target="-blank"><i class="fa-brands fa-instagram"></i></a>
+            </li>
+            <!-- whatsapp 
+            <li style="--clr:#04c214;">
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+            </li>
+        </ul> -->
+       
+        
+    </footer>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="script.js"></script>
+    <!-- scroll bar js  -->
+    <script type="text/javascript">
+        var preogressbar = document .getElementById("preogressbar")
+        var percent = document .getElementById("percent")
+       
+        var totalHeight = document .body.scrollHeight - window.innerHeight;
+        window.onscroll = function(){
+            var progress =( window.pageYOffset / totalHeight) * 100;
+            preogressbar.style.height = progress + "%";
+            percent.innerHTML = "Page Scrolled " + Math.round(progress)+ "%"
+        }
+    </script>
+<!-- pre loader js  -->
+  <script>
+      var loader = document .getElementById("preloader");
+
+      window.addEventListener("load", function(){
+         loader.style.display = "none";
+      })
+  </script>
